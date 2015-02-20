f03b2df0c6fd934e8815c138c3fa15f228d797ee
soyvco <lnxvco@gmail.com>
2015-02-20 13:50:13 -0600

    Creación del proyecto "Inscripcion-Kinder"
    Signed-off-by: soyvco <lnxvco@gmail.com>

---
+++ Inscripcion-Kinder/index.php
@@ -0,0 +1,24 @@
+<!DOCKTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<title>Sistema de registro de alumnos.</title>
+</head>
+<body>
+	<div class="col-md-12 center-block quitar-float text-center">
+		<h1 class="lobster Rojo Grande espacio-arriba centrar">Información del alumno</h1>
+	</div>
+	<div class="col-md-4">
+		<ul class="nav nav-sider lobster mediano">
+			<li><a href="inf-alumno.php">Información del alumno</a></li>
+			<li><a href="calificaciones.php">Calificaciones</a></li>
+		</ul>
+	</div>
+</body>
+</html>
