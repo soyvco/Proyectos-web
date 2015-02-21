09a7ae61ccddfd9c43ccaca6e86f86f93f0a8142
soyvco <lnxvco@gmail.com>
2015-02-21 14:16:50 -0600

    Cambio de sintaxis

--- Inscripcion-Kinder/index.php
+++ Inscripcion-Kinder/index.php
@@ -11,20 +11,20 @@
 	<title>Sistema de registro de alumnos.</title>
 </head>
 <body>
-	<?php
-		echo("<div class='col-md-12 center-block quitar-float text-center'>");
-		echo("<h1 class='lobster Rojo Grande espacio-arriba centrar'>Nombre del kinder</h1>");
-		echo("</div>");
-		echo("<div class='col-md-3'>");
-		echo("<ul class='nav nav-sider lobster mediano'>");
-		$secciones= array("Inicio","¿Quiénes somos?","Ubicación","Inscripciones");
-		$links= array("inicio.php","quienes-somos.php","ubicacion.php","inscripciones.php");
-		$contador=count($secciones);
-		for ($i=0; $i <$contador; $i++) { 
-			echo("<li><a href=''>".$secciones[$i]."</a></li>");
-		}
-		echo("</ul>");
-	?>
+	<div class='col-md-12 center-block quitar-float text-center'>
+		<h1 class='lobster Rojo Grande espacio-arriba centrar'>Nombre del kinder</h1>
+	</div>
+	<div class='col-md-3'>
+		<ul class='nav nav-sider lobster mediano'>
+			<?php
+				$secciones= array("Inicio","¿Quiénes somos?","Ubicación","Inscripciones");
+				$direcciones= array("index.php","quienes-somos.php","ubicacion.php","inscripciones.php");
+				$contador=count($secciones);
+				for ($i=0; $i <$contador; $i++) { 
+					echo("<li><a href='".$direcciones[$i]."'>".$secciones[$i]."</a></li>");
+				}
+			?>
+		</ul>
 	</div>
 </body>
 </html>
